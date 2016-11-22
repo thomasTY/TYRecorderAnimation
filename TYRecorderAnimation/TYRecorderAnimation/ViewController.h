1fd86724487c7ad18f7f9e7edefc7b18a9f366a9
陈天佑 <thomas693@sina.com>
2016-11-22 13:30:58 +0800

    TYRecorderAnimationView

---
+++ TYRecorderAnimation/TYRecorderAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYRecorderAnimation
+//
+//  Created by thomasTY on 16/11/20.
+//  Copyright © 2016年 thomasTY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
